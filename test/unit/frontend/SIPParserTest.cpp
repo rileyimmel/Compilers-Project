fa213e3e9c602c7c1a5ad2223326b2a50f6bf687
Riley Immel <ypq8st@virginia.edu>
2023-09-15 14:33:36 -0400

    Add a failing test for all additions that have a passing test

--- test/unit/frontend/SIPParserTest.cpp
+++ test/unit/frontend/SIPParserTest.cpp
@@ -5,7 +5,7 @@
 
 #include <catch2/catch_test_macros.hpp>
 
-TEST_CASE("SIP Parser: conditionals", "[SIP Parser]") {
+TEST_CASE("SIP Parser: for each", "[SIP Parser]") {
   std::stringstream stream;
   stream << R"(
       short() {
@@ -65,20 +65,79 @@ REQUIRE(ParserHelper::is_parsable(stream));
 
 /************ The following are expected to fail parsing ************/
 
+TEST_CASE("SIP Parser: bad modulo", "[SIP Parser]") {
+std::stringstream stream;
+stream << R"(
+      operators() {
+        var x;
+        x = y % @;
+        return z;
+      }
+    )";
+
+REQUIRE_FALSE(ParserHelper::is_parsable(stream));
+}
+
+TEST_CASE("SIP Parser: bad negation", "[SIP Parser]") {
+std::stringstream stream;
+stream << R"(
+      operators() {
+        var x;
+        x = -@;
+        return z;
+      }
+    )";
+
+REQUIRE_FALSE(ParserHelper::is_parsable(stream));
+}
+
+TEST_CASE("SIP Parser: bad LT", "[SIP Parser]") {
+std::stringstream stream;
+stream << R"(
+      operators() {
+        var x;
+        x = 1 < @;
+        return z;
+      }
+    )";
+
+REQUIRE_FALSE(ParserHelper::is_parsable(stream));
+}
+
+TEST_CASE("SIP Parser: bad LE", "[SIP Parser]") {
+std::stringstream stream;
+stream << R"(
+      operators() {
+        var x;
+        x = 1 <= @;
+        return z;
+      }
+    )";
+
+REQUIRE_FALSE(ParserHelper::is_parsable(stream));
+}
+
+TEST_CASE("SIP Parser: bad GE", "[SIP Parser]") {
+std::stringstream stream;
+stream << R"(
+      operators() {
+        var x;
+        x = 1 >= @;
+        return z;
+      }
+    )";
+
+REQUIRE_FALSE(ParserHelper::is_parsable(stream));
+}
+
 TEST_CASE("SIP Parser: for each with non-expr as second expr", "[SIP Parser]") {
 std::stringstream stream;
 stream << R"(
       short() {
-        var x, y, z;
-        if (x>0) {
-          while (y>z) {
-            y = y + 1;
-          }
-        } else {
+        var x, z;
           for ( x : @ ) {
 						z = z + 1;
 					}
-        }
         return z;
       }
     )";
