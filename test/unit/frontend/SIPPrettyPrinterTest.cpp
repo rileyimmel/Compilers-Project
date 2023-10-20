a672beaa79cf922f0bf266f09c50ff4b90e46e0c
Riley Immel <ypq8st@virginia.edu>
2023-10-20 13:25:49 -0400

    Change test case naming style to match SIP

--- test/unit/frontend/SIPPrettyPrinterTest.cpp
+++ test/unit/frontend/SIPPrettyPrinterTest.cpp
@@ -6,7 +6,7 @@
 
 #include <iostream>
 
-TEST_CASE("PrettyPrinter: Test error stmt printing", "[PrettyPrinter]") {
+TEST_CASE("SIPPrettyPrinter: Test error stmt printing", "[SIPPrettyPrinter]") {
     std::stringstream stream;
     stream << R"(prog() { var x, y; x = 3; y = 2; if(x < y) { error 0;} return 3;})";
 
@@ -31,7 +31,7 @@ TEST_CASE("PrettyPrinter: Test error stmt printing", "[PrettyPrinter]") {
     REQUIRE(ppString == expected);
 }
 
-TEST_CASE("PrettyPrinter: Test inc dec statement printing", "[PrettyPrinter]") {
+TEST_CASE("SIPPrettyPrinter: Test inc dec statement printing", "[SIPPrettyPrinter]") {
     std::stringstream stream;
     stream << R"(prog() { var x, y; x = 0; y = 1; x--; y++; return 0;})";
 
@@ -54,7 +54,7 @@ TEST_CASE("PrettyPrinter: Test inc dec statement printing", "[PrettyPrinter]") {
     REQUIRE(ppString == expected);
 }
 
-TEST_CASE("PrettyPrinter: Test records and record access", "[PrettyPrinter]") {
+TEST_CASE("SIPPrettyPrinter: Test records and record access", "[SIPPrettyPrinter]") {
     std::stringstream stream;
     stream
             << R"(prog() { var x, y, a; a = 3; x = {f1:0, f2:13}; y = x.f1; x.f2 = y + a; return x.f2; })";
@@ -78,10 +78,10 @@ TEST_CASE("PrettyPrinter: Test records and record access", "[PrettyPrinter]") {
     REQUIRE(ppString == expected);
 }
 
-TEST_CASE("PrettyPrinter: Test for range statement", "[PrettyPrinter]") {
+TEST_CASE("SIPPrettyPrinter: Test for range statement", "[SIPPrettyPrinter]") {
     std::stringstream stream;
     stream
-            << R"(prog() { var x, z; z = 5; for (x : 0 .. 4 by 1) { z = z + 1 }  return z;})";
+            << R"(prog() { var x, z; z = 5; for (x : 0 .. 4 by 1) { z = z + 1; }  return z;})";
 
     std::string expected = R"(prog()
 {
@@ -103,10 +103,9 @@ TEST_CASE("PrettyPrinter: Test for range statement", "[PrettyPrinter]") {
     REQUIRE(ppString == expected);
 }
 
-TEST_CASE("PrettyPrinter: Test for range statement with missing step", "[PrettyPrinter]") {
+TEST_CASE("SIPPrettyPrinter: Test for range statement with missing step", "[SIPPrettyPrinter]") {
     std::stringstream stream;
-    stream
-            << R"(prog() { var x, z; z = 5; for (x : 0 .. 4) { z = z + 1 }  return z;})";
+    stream << R"(prog() { var x, z; z = 5; for (x : 0 .. 4) { z = z + 1; }  return z;})";
 
     std::string expected = R"(prog()
 {
@@ -128,7 +127,7 @@ TEST_CASE("PrettyPrinter: Test for range statement with missing step", "[PrettyP
     REQUIRE(ppString == expected);
 }
 
-TEST_CASE("PrettyPrinter: Test ternary operator", "[PrettyPrinter]") {
+TEST_CASE("SIPPrettyPrinter: Test ternary operator", "[SIPPrettyPrinter]") {
     std::stringstream stream;
     stream
             << R"(prog() { var x, y, z; x = 3; y = 4; z = (x < y) ? 2 : 3; return (x > y) ? 3 : 1;})";
@@ -151,7 +150,7 @@ TEST_CASE("PrettyPrinter: Test ternary operator", "[PrettyPrinter]") {
     REQUIRE(ppString == expected);
 }
 
-TEST_CASE("PrettyPrinter: Test bool expressions", "[PrettyPrinter]") {
+TEST_CASE("SIPPrettyPrinter: Test bool expressions", "[SIPPrettyPrinter]") {
     std::stringstream stream;
     stream
             << R"(prog() { var x, z; x = true; z = false;  return z;})";
@@ -173,7 +172,7 @@ TEST_CASE("PrettyPrinter: Test bool expressions", "[PrettyPrinter]") {
     REQUIRE(ppString == expected);
 }
 
-TEST_CASE("PrettyPrinter: Test RefExpr printing", "[PrettyPrinter]") {
+TEST_CASE("SIPPrettyPrinter: Test RefExpr printing", "[SIPPrettyPrinter]") {
     std::stringstream stream;
     stream << R"(prog() { var x, y; y = &x; return 0;})";
 
@@ -193,7 +192,7 @@ TEST_CASE("PrettyPrinter: Test RefExpr printing", "[PrettyPrinter]") {
     REQUIRE(ppString == expected);
 }
 
-TEST_CASE("PrettyPrinter: Test array, indexing, and length", "[PrettyPrinter]") {
+TEST_CASE("SIPPrettyPrinter: Test array, indexing, and length", "[SIPPrettyPrinter]") {
 std::stringstream stream;
 stream << R"(prog(){var x, y, z, a; a = [[1,2,3],[4,5,6]]; y = a[0][1]; z = [7,8,9]; y = z[2]; x = ##a; return #z;})";
 
@@ -217,7 +216,7 @@ expected = GeneralHelper::removeTrailingWhitespace(expected);
 REQUIRE(ppString == expected);
 }
 
-TEST_CASE("PrettyPrinter: Test array of operator", "[PrettyPrinter]") {
+TEST_CASE("SIPPrettyPrinter: Test array of operator", "[SIPPrettyPrinter]") {
 std::stringstream stream;
 stream << R"(prog(){var x, a; a = [1,2,3]; x = [5 of a[1]]; return 0;})";
 
@@ -238,7 +237,7 @@ expected = GeneralHelper::removeTrailingWhitespace(expected);
 REQUIRE(ppString == expected);
 }
 
-TEST_CASE("PrettyPrinter: Test unary not operator", "[PrettyPrinter]") {
+TEST_CASE("SIPPrettyPrinter: Test unary not operator", "[SIPPrettyPrinter]") {
 std::stringstream stream;
 stream << R"(prog(){var x; x = 0; return not x;})";
 
@@ -258,9 +257,9 @@ expected = GeneralHelper::removeTrailingWhitespace(expected);
 REQUIRE(ppString == expected);
 }
 
-TEST_CASE("PrettyPrinter: Test for each statement", "[PrettyPrinter]") {
+TEST_CASE("SIPPrettyPrinter: Test for each statement", "[SIPPrettyPrinter]") {
 std::stringstream stream;
-stream << R"(prog(){var x, y, z for (x : x){z = z + 1 - 3;}return z;})";
+stream << R"(prog(){ var x, y, z; for (x : x) {z = z + 1 - 3; } return z;})";
 
 std::string expected = R"(prog()
 {
@@ -281,7 +280,7 @@ expected = GeneralHelper::removeTrailingWhitespace(expected);
 REQUIRE(ppString == expected);
 }
 
-TEST_CASE("PrettyPrinter: Test multiple functions", "[PrettyPrinter]") {
+TEST_CASE("SIPPrettyPrinter: Test multiple functions", "[SIPPrettyPrinter]") {
 std::stringstream stream;
 stream << R"(foo(f, a){ return f(a);} bar(x){ return x + 1;} baz(y){ return foo(bar, y);} main(){ var z; z = 3; return baz(z);})";
 
